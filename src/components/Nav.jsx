e2c2ccb1c3d9fa61600bdba5fb7dbcbbfcd49459
MatthewKuria <mathewkwachira@gmail.com>
2023-12-06 17:46:22 +0300

    Add a responsive navbar using state variable

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,57 +1,99 @@
-import React from 'react';
+import React, {useState} from 'react';
 export default function Nav (){
-    return(
-        // <header>
-        //     <nav >
-        //        <div className=' container mx-auto  flex '>
-        //        <h1 className='text-green-600 font-bold  text-4xl mx-auto '>The-Big-5</h1>
-        //        <div>
-        //         <ul className='text-gray-400 font-semibold flex  justify-between inline-block '>
-        //             <li className='hover:text-green-400'><a href="#">Elephants</a></li>
-        //             <li className='hover:text-green-400'><a href="">Rhino</a></li>
-        //             <li className='hover:text-green-400'><a href="">Buffalo</a></li>
-        //             <li className='hover:text-green-400'><a href="">Lion</a></li>
-        //             <li className='hover:text-green-400'><a href="">Leopard</a></li>
-        //         </ul>
-        //        </div>
-        //        </div>
-        //     </nav>
-        // </header>
-        
+  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
 
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-        {/* <img src="" class="h-8" alt="Nav Logo" /> */}
-        <span class="self-center text-2xl font-semibold whitespace-nowrap text-green-600 dark:text-white">The-Big-5</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page"><em>Elephants</em></a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Rhino</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Bufallo</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Lions</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-green-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Leopard</a>
-        </li>
-      </ul>
+  return (
+    <div className="flex items-center justify-between border-b border-gray-400 py-8">
+      <a href="/">
+        {/* <img src="" alt="logo" /> */}
+        <span><h1 className='text-green-600 font-bold uppercase text-2xl'>The-Big-5</h1></span>
+      </a>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}> 
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/elephants">Elephants</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/rhino">Rhino</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/bufallo">Bufallo</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/lion">Lion</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/leopard">Leopard</a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex text-gray-600 font-bold ">
+          <li className='hover:text-green-600'>
+          <a href="/elephants">Elephants</a>
+          </li>
+          <li className='hover:text-green-600'>
+          <a href="/rhino">Rhino</a>
+          </li>
+          <li className='hover:text-green-600'>
+          <a href="/bufallo">Bufallo</a>
+          </li>
+          <li className='hover:text-green-600'>
+          <a href="/lion">Lion</a>
+          </li>
+          <li className='hover:text-green-600'>
+          <a href="/leopard">Leopard</a>
+          </li>
+        </ul>
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
     </div>
-  </div>
-</nav>
-
-    )
+  );
 }
