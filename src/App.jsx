99418086f697ec87a34512fb63a97b62e0eeb8e5
MatthewKuria <mathewkwachira@gmail.com>
2023-12-08 13:30:25 +0300

    added all animals

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react';
 import Nav from './components/Nav';
 import Card from './components/Card'
 import './App.css'
+import animalsData from './animalsData';
 
 function App() {
- 
+  //populate the card using data
+  const animalElement = animalsData.map(animal =>{
+    return(
+      <Card 
+      key={animal.id}
+      name={animal.name}
+      img={animal.img}
+      more={animal.more}
+      />
+    )
+  })
 
   return (
     <div className='relative'>
     <Nav  />
     <h1 className="text-green-600 font-bold my-10">The Big 5 Animals</h1>
-      <Card />
+      {/* Display animal in a card */}
+      {animalElement}
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 
-export default function Card (){
+export default function Card (props){
     return(
-        <div className="  columns-1  md:columns-2 box-content p-4 border-2">
-            <img className="aspect-[4/3] rounded  object-cover  h-80 w-110  flex-1" src="../elephant.jpeg" alt="elephant " />
-            <p className="w-full">This is an elephant it is among the big Five animals in Kenya.
-            An African Savanna elephant can grow to 30 ft. (9 meters) from trunk to tail, 
-            weigh more than 13,000 lbs. (6 tons/5,442 kilograms) and stand as tall as 12 ft. (4 meters)
-             at the shoulder. 
-             The African Savanna elephant is classified as an Endangered Species.
+        <div  className="  columns-1  md:columns-2 box-content p-4 border-2">
+            <h1 className="text-green-600 font-semibold">{props.name}</h1>
+            <img className="aspect-[4/3] rounded  object-cover  h-80 w-110  flex-1" src={props.img} alt="elephant " />
+            <p className="w-full">{props.more}
              <button className=" block bg-green-600 text-white font-bold mx-auto my-9 py-3 px-2 border-2 hover:bg-white hover:text-green-600 hover:border-green-600" >
                 <a href="/gallery">
                     View Gallery
